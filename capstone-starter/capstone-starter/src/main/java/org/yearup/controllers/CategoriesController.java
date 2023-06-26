993ab460c1d021129efbbc1a733f90ee765220df
ebellerand <ebellerand@appdev.yearup.org>
2023-06-26 02:12:50 -0400

    Added functionality to searchcategorybyid

--- capstone-starter/capstone-starter/src/main/java/org/yearup/controllers/CategoriesController.java
+++ capstone-starter/capstone-starter/src/main/java/org/yearup/controllers/CategoriesController.java
@@ -24,14 +24,14 @@ import java.util.List;
 @CrossOrigin
 @RequestMapping("/categories")
 @RestController
-public class CategoriesController
-{
+public class CategoriesController {
     private CategoryDao categoryDao;
     private ProductDao productDao;
 
-@Autowired
+    @Autowired
     private DataSource dataSource;
-@Autowired
+
+    @Autowired
     public CategoriesController(CategoryDao categoryDao, ProductDao productDao, DataSource dataSource) {
         this.categoryDao = categoryDao;
         this.productDao = productDao;
@@ -42,67 +42,92 @@ public class CategoriesController
 
     // add the appropriate annotation for a get action
     @GetMapping
-    public List<Category> getAll()
-    {
+    public List<Category> getAll() {
         // find and return all categories
         List<Category> allCategories = new ArrayList<>();
 
-            String query = "SELECT * FROM categories";
-            try (Connection connection = dataSource.getConnection()) {
-                PreparedStatement preparedStatement = connection.prepareStatement(query);
-                ResultSet resultSet = preparedStatement.executeQuery();
+        String query = "SELECT * FROM categories";
+        try (Connection connection = dataSource.getConnection()) {
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            ResultSet resultSet = preparedStatement.executeQuery();
 
-                while(resultSet.next()){
-                    int categoryid = resultSet.getInt("category_id");
-                    String name = resultSet.getString("name");
-                    String description = resultSet.getString("description");
-                    Category category = new Category(categoryid, name, description);
-                    allCategories.add(category);
+            while (resultSet.next()) {
+                int categoryid = resultSet.getInt("category_id");
+                String name = resultSet.getString("name");
+                String description = resultSet.getString("description");
+                Category category = new Category(categoryid, name, description);
+                allCategories.add(category);
 
 
-                }
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
             }
-            return allCategories;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return allCategories;
     }
 
     // add the appropriate annotation for a get action
-    public Category getById(@PathVariable int id)
-    {
-        // get the category by id
+
+    @GetMapping("/{id}")
+    public Category getById(@PathVariable int id) {
+        Category category = null;
+
+
+        String query = "SELECT * FROM categories WHERE category_id = ?";
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+
+            preparedStatement.setInt(1, id);
+
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    category = extractCategoryFromResultSet(resultSet);
+                    return category;
+                }
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+            // get the category by id
+        } catch (SQLException ex) {
+            throw new RuntimeException(ex);
+        }
         return null;
     }
 
     // the url to return all products in category 1 would look like this
     // https://localhost:8080/categories/1/products
     @GetMapping("{categoryId}/products")
-    public List<Product> getProductsById(@PathVariable int categoryId)
-    {
+    public List<Product> getProductsById(@PathVariable int categoryId) {
         // get a list of product by categoryId
         return null;
     }
 
     // add annotation to call this method for a POST action
     // add annotation to ensure that only an ADMIN can call this function
-    public Category addCategory(@RequestBody Category category)
-    {
+    public Category addCategory(@RequestBody Category category) {
         // insert the category
         return null;
     }
 
     // add annotation to call this method for a PUT (update) action - the url path must include the categoryId
     // add annotation to ensure that only an ADMIN can call this function
-    public void updateCategory(@PathVariable int id, @RequestBody Category category)
-    {
+    public void updateCategory(@PathVariable int id, @RequestBody Category category) {
         // update the category by id
     }
 
 
     // add annotation to call this method for a DELETE action - the url path must include the categoryId
     // add annotation to ensure that only an ADMIN can call this function
-    public void deleteCategory(@PathVariable int id)
-    {
+    public void deleteCategory(@PathVariable int id) {
         // delete the category by id
+    }
+
+    private Category extractCategoryFromResultSet(ResultSet resultSet) throws Exception {
+
+            int id = resultSet.getInt("category_id");
+            String name = resultSet.getString("name");
+            String description = resultSet.getString("description");
+            Category category = new Category(id, name, description);
+            return category;
     }
 }
